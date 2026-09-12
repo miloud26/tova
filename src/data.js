0b691fa0e5d14e78c7cbddd4d833d758e6a3a578
miloud26 <fiverr.miloud@gmail.com>
2026-09-12 10:40:43 +0100

    fix speed

--- src/data.js
+++ src/data.js
@@ -9,9 +9,8 @@ export const data = [
     delevery: "",
     clr: [],
     size: [],
-    descImag1: "https://i.ibb.co/Tqw7q1KJ/output.webp",
-    themImg:
-      "https://i0.wp.com/www.tova-dz.com/wp-content/uploads/2026/05/ChatGPT-WPD-tova.webp?resize=500,500&ssl=1",
+    descImag1: "https://i.ibb.co/gZMQdtR5/desc.webp",
+    themImg: "https://i.ibb.co/3y2XLB7L/thum.webp",
     url: "https://script.google.com/macros/s/AKfycbyUERf3Ka9jwUlZZsCfZAMkPOupYP6TR8192hVRJYIhbQPV-ykV5qSbr5InFYpCZQjOqg/exec",
   },
 ];
